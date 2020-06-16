8966fcc9d77b7aa67f09e153c31d60112a8e8283
Ralph Robert Hossepian <ralphhovsepian@mail.ru>
2020-06-16 14:27:44 +0400

    Updates

---
+++ precache-manifest.c8154871279c88f93b31b5c3914e5d73.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7e5d839839465f369bbc9d9a0c8c176",
+    "url": "/whereintheworld/index.html"
+  },
+  {
+    "revision": "73aab9aface8869b36f3",
+    "url": "/whereintheworld/static/css/main.e64b84ed.chunk.css"
+  },
+  {
+    "revision": "6a73416f95558c497193",
+    "url": "/whereintheworld/static/js/2.841ba76a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/whereintheworld/static/js/2.841ba76a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "73aab9aface8869b36f3",
+    "url": "/whereintheworld/static/js/main.991fcc25.chunk.js"
+  },
+  {
+    "revision": "9a3b75af4d84b60b1104",
+    "url": "/whereintheworld/static/js/runtime-main.3c803550.js"
+  }
+]);
